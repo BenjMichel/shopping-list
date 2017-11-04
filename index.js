dd6248ae14ea2835f2a7e9f94eb5c673cc707f98
Benjamin Michel <bmichel@tilli.fr>
2017-11-04 01:06:21 +0100

    Display photo in background and footer button

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-import './style';
 import { Component } from 'preact';
+import './style.css';
+import photo from './assets/photo1.jpg'
 
 export default class App extends Component {
-	render() {
-		return (
-			<div>
-				<h1>Hello, World!</h1>
-			</div>
-		);
-	}
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  renderHome() {
+    return (
+      <div className="container" style={{ backgroundImage: `url(${photo})` }}>
+        <h1>Shopping list!</h1>
+        <div className="footer" onClick={() => this.setState({ showAdd: true })}>
+          ajouter
+        </div>
+      </div>
+    );
+  }
+
+  renderAdd() {
+    return (
+      <div>Hello</div>
+    );
+  }
+
+  render() {
+    if (this.state.showAdd) {
+      return this.renderAdd();
+    }
+    return this.renderHome();
+  }
 }
