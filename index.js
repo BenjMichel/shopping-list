84df67981b97c648e9b90eb9dde73e54d4ecee98
Benjamin Michel <bmichel@tilli.fr>
2017-11-04 01:27:52 +0100

    Random background photo

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 import { Component } from 'preact';
 import './style.css';
-import photo from './assets/photo1.jpg'
+import photo1 from './assets/photo1.jpg'
+import photo2 from './assets/photo2.jpg'
+import photo3 from './assets/photo3.jpg'
+import photo4 from './assets/photo4.jpg'
+
+const photos = [photo1, photo2, photo3, photo4];
 
 export default class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      photo: photos[Math.floor(Math.random() * photos.length)]
+    };
   }
 
   renderHome() {
     return (
-      <div className="container" style={{ backgroundImage: `url(${photo})` }}>
+      <div className="container">
+        <div className="background" style={{ backgroundImage: `url(${this.state.photo})` }} />
         <h1>Shopping list!</h1>
         <div className="footer" onClick={() => this.setState({ showAdd: true })}>
           ajouter
@@ -21,7 +29,13 @@ export default class App extends Component {
 
   renderAdd() {
     return (
-      <div>Hello</div>
+      <div className="container">
+        <div className="background" style={{ backgroundImage: `url(${this.state.photo})` }} />
+        <input className="input" />
+        <div className="footer" onClick={() => this.setState({ showAdd: true })}>
+          ajouter
+        </div>
+      </div>
     );
   }
 
